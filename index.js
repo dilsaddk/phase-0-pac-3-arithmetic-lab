d9d2fc33e3ed45d422487a57377b9e0435b2e636
dilsaddk <kurdakdilsad@gmail.com>
2023-06-16 12:14:40 +0300

    changed index.js file

---
+++ index.js
@@ -0,0 +1,20 @@
+/* const myNumber = '3';
+console.log(2 + parseInt(myNumber));
+// returns 5
+
+const age = 'age is 50';
+console.log(parseInt(age));
+// returns NaN
+
+const age = '50 is the age';
+console.log(parseInt(age));
+// returns 50
+
+console.log(parseInt("50", 12));
+// returns 60 */
+
+console.log(parseInt("6.2927", 10));
+
+console.log(parseInt("nonsense!", 10));
+
+console.log(parseFloat("70.56478"));
